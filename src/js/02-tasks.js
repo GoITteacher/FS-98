d5119711ad57f86b41aa8e0d0c553f1086c33bbe
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-11-16 22:10:29 +0200

    update

--- src/js/02-tasks.js
+++ src/js/02-tasks.js
@@ -7,8 +7,14 @@
  * Використовуй конструкцію if...else.
  */
 
-let link = "https://my-site.com/about";
-console.log(link);
+// let link = 'https://my-site.com/about';
+// const isCorrectLink = link.endsWith('/');
+
+// if (!isCorrectLink) {
+//   link += '/END';
+// }
+
+// console.log(link);
 
 /**
  * Форматування посилання (includes та логічне «І»)
@@ -19,17 +25,28 @@ console.log(link);
  * Використовуй конструкцію if...else або тернарний оператор.
  */
 
-let url = "https://somesite.com/about";
+let url = 'https://my-site.com/about';
+
+const isCorrectEnd = url.endsWith('/');
+const hasMySite = url.includes('my-site');
+
+if (!isCorrectEnd && hasMySite) {
+  url += '/';
+}
+
 console.log(url);
 
 /*
  * Пошук у рядку методом includes()
  */
 
-const blacklistedWord1 = "spam";
-const blacklistedWord2 = "sale";
+// const blacklistedWord1 = 'spam';
+// const blacklistedWord2 = 'sale';
 
-const string1 =
-  "Hello, I'm Prince Abdul, this is not spam, I'm offering you a million!";
-const string2 = "Biggest SALE this week, don't miss out!";
-const string3 = "#fatlivesmatter advertising campaign";
+// const string1 =
+//   "Hello, I'm Prince Abdul, this is not spam, I'm offering you a million!";
+// const string2 = "Biggest SALE this week, don't miss out!";
+// const string3 = '#fatlivesmatter advertising campaign';
+
+// string1.toLowerCase().includes(blacklistedWord1) ||
+// string1.toLowerCase().includes(blacklistedWord2);
--- src/js/03-loops.js
+++ src/js/03-loops.js
@@ -9,23 +9,91 @@
  * - Цикли while і do...while
  */
 
-console.log("Before");
+// console.log('Before');
+// const str = 'HELLO WORLD';
 
-console.log("After");
+// i = 1
+// for (let i = 10; i >= 0; i -= 1) {
+//   console.log(str[i]);
+// }
+// for (let i = 0; i < 50; i += 1) {}
+
+// for (let i = 50; i >= 0; i -= 1) {}
+
+// let x = 10;
+
+// console.log(x++);
+// console.log(x);
+
+// console.log('After');
+
+// let total = 0;
+// for (let i = 0; i < 5; i++) {
+//   const num = Number(prompt('Enter number'));
+//   total += num;
+// }
+// console.log(total);
+
+/**
+ * -------------------------------
+ */
+function myGame() {
+  const random = Math.round(Math.random() * 1000);
+  let num;
+
+  while (num != random) {
+    num = +prompt('x = ');
+    if (num > random) {
+      console.log('<');
+    } else {
+      console.log('>');
+    }
+  }
+}
+
+// function myGameV2() {
+//   const random = Math.round(Math.random() * 1000);
+
+//   do {
+//     let num = +prompt('x = ');
+//     if (num > random) {
+//       console.log('<');
+//     } else {
+//       console.log('>');
+//     }
+//   } while (num != random);
+// }
+
+// myGame();
+// console.log('After');
+
+// ==============
+
+// while (false) {
+//   console.log('Hello');
+// }
+
+// const dreams = 1500;
+// let salary = 1700;
+
+// while (salary < dreams) {
+//   console.log('More', salary);
+//   salary += 170;
+// }
+// console.log(salary);
+
+// do {
+//   console.log('More', salary);
+//   salary += 170;
+// } while (salary < dreams);
+
+// console.log(salary);
 
 /**
  * -------------------------------
  */
 
-console.log("Before");
-
-console.log("After");
-
-/**
- * -------------------------------
- */
-
-let a = 10;
+// let a = 10;
 
 // console.log(a);
 // console.log(b);
@@ -34,8 +102,32 @@ let a = 10;
  * -------------------------------
  */
 
-console.log("Before while loop");
+// console.log('Before while loop');
 
-let counter = 0;
+// let counter = 0;
 
-console.log("After while loop");
+// console.log('After while loop');
+
+// ============================================
+
+// for (let i = 1; i < 50; i++) {
+//   if (i % 5 === 0 && i % 6 === 0) continue;
+//   console.log(i);
+// }
+
+// const str = 'Hello world';
+// let index = -1;
+
+// for (let i = 0; i < str.length; i++) {
+//   console.log(i);
+//   if (str[i] === 'o' && index === -1) {
+//     index = i;
+//     break;
+//   }
+// }
+
+// const str = 'Hello World';
+
+// for (let i = 0; i < str.length; i++) {
+//   if (str[i] === 'H' || str[i] === 'W') continue;
+// }
