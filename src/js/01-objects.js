bc800e76e3e57fada8a708b076e67af894bee510
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-11-28 18:51:40 +0200

    1

--- src/js/01-objects.js
+++ src/js/01-objects.js
@@ -10,23 +10,10 @@
  * - Додавання властивостей
  */
 
-const playlist = {
-  name: "My amazing playlist",
-  rating: 5,
-  tracks: ["track-1", "track-2", "track-3"],
-  isFavourite: true,
-};
+/**
+ * -----------------------------------
+ */
 
 /**
  * -----------------------------------
  */
-const propertyName = "tracks";
-
-/**
- * -----------------------------------
- */
-function foo(values) {
-  console.log(values);
-  console.log(values.a);
-  console.log(values.b);
-}
