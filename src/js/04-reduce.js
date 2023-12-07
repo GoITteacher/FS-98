a7368ac72a502a4170eede7c6c6127e8d3c4311d
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-12-07 22:27:41 +0200

    update

--- src/js/04-reduce.js
+++ src/js/04-reduce.js
@@ -5,9 +5,25 @@
  * - Значення, що повертається, залежить від розробника
  */
 
-const numbers = [5, 10, 15, 20, 25];
-const total = numbers;
-console.log(total);
+// =======
+// const numbers = [5, 10, 15, 20, 25];
+
+// const result = numbers.reduce((acc, elem, idx, arr) => {
+//   if (elem % 2 === 0) {
+//     acc.push(elem);
+//   }
+//   return acc;
+// }, []);
+
+// console.log(result);
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// const result = numbers.reduce((acc, elem) => {
+//   return acc && elem % 5 === 0;
+// }, true);
+
+// console.log(result);
 
 /**
  * Рахуємо загальну зарплату
@@ -19,32 +35,42 @@ const salary = {
   ajax: 150,
 };
 
-const totalSalary = salary;
-console.log(totalSalary);
+// [100,50,150]
+
+// const totalSalary = Object.values(salary).reduce((sum, elem) => {
+//   return sum + elem;
+// }, 0);
+
+// console.log(totalSalary);
 
 /**
  * Рахуємо загальну кількість годин
  */
 
-const players = [
-  { id: "player-1", name: "Mango", timePlayed: 310, online: false },
-  { id: "player-2", name: "Poly", timePlayed: 470, online: true },
-  { id: "player-3", name: "Kiwi", timePlayed: 230, online: true },
-  { id: "player-4", name: "Ajax", timePlayed: 150, online: false },
-  { id: "player-5", name: "Chelsey", timePlayed: 80, online: true },
+/* const players = [
+  { id: 'player-1', name: 'Mango', timePlayed: 310, online: false },
+  { id: 'player-2', name: 'Poly', timePlayed: 470, online: true },
+  { id: 'player-3', name: 'Kiwi', timePlayed: 230, online: true },
+  { id: 'player-4', name: 'Ajax', timePlayed: 150, online: false },
+  { id: 'player-5', name: 'Chelsey', timePlayed: 80, online: true },
 ];
 
-const totalTimePlayed = players;
-console.log(totalTimePlayed);
+const totalTimePlayed = players.reduce((total, player, i, arr) => {
+  return total + player.timePlayed / arr.length;
+}, 0);
+
+console.log(totalTimePlayed); */
 
 /**
  * Рахуємо загальну суму товарів кошика
  */
-const cart = [
-  { label: "Apples", price: 100, quantity: 2 },
-  { label: "Bananas", price: 120, quantity: 3 },
-  { label: "Lemons", price: 70, quantity: 4 },
+
+/* const cart = [
+  { label: 'Apples', price: 100, quantity: 2 },
+  { label: 'Bananas', price: 120, quantity: 3 },
+  { label: 'Lemons', price: 70, quantity: 4 },
 ];
 
 const totalAmount = cart;
 console.log(totalAmount);
+ */
--- src/js/05-sort.js
+++ src/js/05-sort.js
@@ -10,19 +10,26 @@
  * - приводить елементи до рядка і сортує за [Unicode](https://unicode-table.com/en/)
  */
 
-const numbers = [1, 9, 6, 2, 3];
+// const numbers = [1, 6, 9, 6, 3];
 
-const sorted = numbers;
-console.log("sorted ", sorted);
+// const result = numbers.toSorted((a, b) => {
+//   return a - b;
+// });
 
-const letters = ["b", "B", "a", "A"];
-console.log("letters", letters);
+// console.log(result);
+
+// const sorted = numbers;
+// console.log("sorted ", sorted);
+
+// const letters = ["b", "B", "a", "A"];
+// console.log("letters", letters);
 
 /**
  * compareFunction - функція порівняння (callback)
  * Елементи масиву сортуються відповідно до її значення, що повертається
  */
 
+/* 
 console.log(
   numbers.toSorted((curEl, nextEl) => {
     return nextEl - curEl;
@@ -32,21 +39,38 @@ console.log(
 const descSortedNumbers = numbers;
 const ascSortedNumbers = numbers;
 console.log("descSortedNumbers", descSortedNumbers);
-console.log("ascSortedNumbers", ascSortedNumbers);
+console.log("ascSortedNumbers", ascSortedNumbers); */
 
 /**
  * Сортування масиву об'єктів
  */
-const players = [
-  { id: "player-1", name: "Mango", timePlayed: 310, online: false },
-  { id: "player-2", name: "Poly", timePlayed: 470, online: true },
-  { id: "player-3", name: "Aiwi", timePlayed: 230, online: true },
-  { id: "player-4", name: "Ajax", timePlayed: 150, online: false },
-  { id: "player-5", name: "Chelsey", timePlayed: 80, online: true },
-];
+// const players = [
+//   { id: 'player-1', name: 'Test', timePlayed: 310, online: false },
+//   { id: 'player-2', name: 'Mango', timePlayed: 470, online: true },
+//   { id: 'player-3', name: 'Ajax', timePlayed: 230, online: true },
+//   { id: 'player-4', name: 'Ajax', timePlayed: 150, online: false },
+//   { id: 'player-5', name: 'Mango', timePlayed: 80, online: true },
+//   { id: 'player-5', name: 'Test', timePlayed: 80, online: true },
+//   { id: 'player-5', name: 'Mango', timePlayed: 80, online: true },
+// ];
+
+// const result = players.toSorted((player2, player1) => {
+//   return player1.timePlayed - player2.timePlayed;
+// });
+
+// const result2 = players.toSorted((p1, p2) => {
+//   if (p1.name === p2.name) {
+//     return p1.timePlayed - p2.timePlayed;
+//   } else {
+//     return p1.name.localeCompare(p2.name);
+//   }
+// });
+
+// console.table(result2);
 
 // За ігровим часом
-const sortedByBestPlayers = players;
+
+/* const sortedByBestPlayers = players;
 console.table(sortedByBestPlayers);
 
 const sortedByWorstPlayers = players;
@@ -54,4 +78,26 @@ console.table(sortedByWorstPlayers);
 
 // По первой букве имени
 const byName = players;
-console.table(byName);
+console.table(byName); */
+
+// ==================
+// const x = 'abcT';
+// const y = 'abcY';
+// console.log(x.localeCompare(y));
+// ==================================
+
+// const arr = [3, 5, 2, 4, 7, 1, 8, 0];
+
+// for (let i = 0; i < arr.length; i += 1) {
+//   for (let j = i; j < arr.length; j++) {
+//     if (arr[i] > arr[j]) {
+//       const temp = arr[i];
+//       arr[i] = arr[j];
+//       arr[j] = temp;
+//     }
+//   }
+// }
+
+// console.log(arr);
+
+// ===========
