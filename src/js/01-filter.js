a1650f0f7392bb373581c1d27cb2261bf7250471
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-12-07 20:50:11 +0200

    update

--- src/js/01-filter.js
+++ src/js/01-filter.js
@@ -1,3 +1,16 @@
+// const arr = [1, 2, 3, 4, 5, 6, 7, 6];
+// const result = arr.map(elem => {
+//   return '+' + elem;
+// });
+
+// console.log(result);
+
+// const result = arr.forEach((elem, idx, array) => {
+//   console.log(idx, elem);
+// });
+
+// console.log('Result', result);
+
 /**
  * Метод filter
  *
@@ -8,91 +21,92 @@
  *    - якщо коллбек повернув false елемент НЕ додається в масив, що повертається
  */
 
-const numbers = [5, 10, 15, 20, 25];
+const numbers = [5, 10, 15, 20, 25, 22];
 
-const filteredNumbers = numbers;
-console.log(filteredNumbers);
+const result = numbers.filter(elem => elem > 15);
+
+console.log(result);
 
 /**
  * ---------------------------
  */
 const allCars = [
   {
-    make: "Honda",
-    model: "CR-V",
-    type: "suv",
+    make: 'Honda',
+    model: 'CR-V',
+    type: 'suv',
     amount: 14,
     price: 24045,
     onSale: true,
   },
   {
-    make: "Honda",
-    model: "Accord",
-    type: "sedan",
+    make: 'Honda',
+    model: 'Accord',
+    type: 'sedan',
     amount: 2,
     price: 22455,
     onSale: true,
   },
   {
-    make: "Mazda",
-    model: "Mazda 6",
-    type: "sedan",
+    make: 'Mazda',
+    model: 'Mazda 6',
+    type: 'sedan',
     amount: 8,
     price: 24195,
     onSale: false,
   },
   {
-    make: "Mazda",
-    model: "CX-9",
-    type: "suv",
+    make: 'Mazda',
+    model: 'CX-9',
+    type: 'suv',
     amount: 7,
     price: 31520,
     onSale: true,
   },
   {
-    make: "Toyota",
-    model: "4Runner",
-    type: "suv",
+    make: 'Toyota',
+    model: '4Runner',
+    type: 'suv',
     amount: 19,
     price: 34210,
     onSale: false,
   },
   {
-    make: "Toyota",
-    model: "Sequoia",
-    type: "suv",
+    make: 'Toyota',
+    model: 'Sequoia',
+    type: 'suv',
     amount: 16,
     price: 45560,
     onSale: false,
   },
   {
-    make: "Toyota",
-    model: "Tacoma",
-    type: "truck",
+    make: 'Toyota',
+    model: 'Tacoma',
+    type: 'truck',
     amount: 4,
     price: 24320,
     onSale: true,
   },
   {
-    make: "Ford",
-    model: "F-150",
-    type: "truck",
+    make: 'Ford',
+    model: 'F-150',
+    type: 'truck',
     amount: 11,
     price: 27110,
     onSale: true,
   },
   {
-    make: "Ford",
-    model: "Fusion",
-    type: "sedan",
+    make: 'Ford',
+    model: 'Fusion',
+    type: 'sedan',
     amount: 13,
     price: 22120,
     onSale: true,
   },
   {
-    make: "Ford",
-    model: "Explorer",
-    type: "suv",
+    make: 'Ford',
+    model: 'Explorer',
+    type: 'suv',
     amount: 6,
     price: 31660,
     onSale: false,
@@ -104,7 +118,9 @@ const allCars = [
  * значення параметра threshold.
  */
 
-const filterByPrice = (cars, threshold) => {};
+// const filterByPrice = (cars, threshold) => {
+//   return cars.filter(car => car.price > threshold);
+// };
 
 // console.table(filterByPrice(allCars, 30000));
 // console.table(filterByPrice(allCars, 25000));
@@ -114,7 +130,20 @@ const filterByPrice = (cars, threshold) => {};
  * властивість onSale яких true.
  */
 
-const getCarsWithDiscount = (cars) => {};
+/* 
+ {
+    make: 'Honda',
+    model: 'CR-V',
+    type: 'suv',
+    amount: 14,
+    price: 24045,
+    onSale: true,
+  }
+*/
+
+// const getCarsWithDiscount = cars => {
+//   return cars.filter(car => !car.onSale);
+// };
 
 // console.table(getCarsWithDiscount(allCars));
 
@@ -123,7 +152,11 @@ const getCarsWithDiscount = (cars) => {};
  * зі значенням параметра type.
  */
 
-const getCarsWithType = (cars, type) => {};
+// const getCarsWithType = (cars, type) => {
+//   return cars.filter(car => {
+//     return car.type === type;
+//   });
+// };
 
-// console.table(getCarsWithType(allCars, "suv"));
-// console.table(getCarsWithType(allCars, "sedan"));
+// console.table(getCarsWithType(allCars, 'suv'));
+// console.table(getCarsWithType(allCars, 'sedan'));
--- src/js/03-every-some.js
+++ src/js/03-every-some.js
@@ -1,10 +1,12 @@
 const players = [
-  { id: "player-1", name: "Mango", timePlayed: 310, points: 54, online: true },
-  { id: "player-2", name: "Poly", timePlayed: 470, points: 92, online: false },
-  { id: "player-3", name: "Kiwi", timePlayed: 230, points: 48, online: true },
-  { id: "player-4", name: "Ajax", timePlayed: 150, points: 71, online: false },
-  { id: "player-5", name: "Chelsy", timePlayed: 280, points: 48, online: true },
+  { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: true },
+  { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: false },
+  { id: 'player-3', name: 'Kiwi', timePlayed: 50, points: 48, online: true },
+  { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+  { id: 'player-5', name: 'Chelsy', timePlayed: 280, points: 48, online: true },
 ];
+
+// console.log(result);
 
 /**
  * Метод every
@@ -12,7 +14,12 @@ const players = [
  * - Поелементно перебирає оригінальний масив
  * - Повертає true якщо всі елементи масиву задовольняють умову
  */
-const isAllOnline = players;
+
+// const isAllOnline = players.every(player => player.online);
+
+// const isAllTime = players.every(player => player.timePlayed >= 100);
+
+// console.log(isAllTime);
 // console.log('isAllOnline: ', isAllOnline);
 
 /**
@@ -22,7 +29,13 @@ const isAllOnline = players;
  * - Повертає true якщо хоча б один елемент масиву задовольняє умову
  */
 const isAnyOnline = players;
-console.log("isAnyOnline: ", isAnyOnline);
+// console.log('isAnyOnline: ', isAnyOnline);
 
 const anyHardcorePlayers = players;
-console.log("anyHardcorePlayers: ", anyHardcorePlayers);
+// console.log('anyHardcorePlayers: ', anyHardcorePlayers);
+
+const result = players.some(player => {
+  return player.points > 90;
+});
+
+console.log(result);
