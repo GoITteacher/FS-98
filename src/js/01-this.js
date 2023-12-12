96ace493fa606d5fc74cc217e6871c878d709933
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-12-12 19:04:48 +0200

    update

--- src/js/01-this.js
+++ src/js/01-this.js
@@ -9,78 +9,19 @@
 /**
  * Глобальний контекст
  */
-function foo() {
-  console.log('foo -> this', this);
-}
-
-foo();
 
 /**
  * Контекст методу об'єкта
  */
 
-const user = {
-  tag: 'Mango',
-  showTag() {
-    console.log('showTag -> this', this);
-  },
-};
-
-user.showTag();
-
 /**
- * Контекст методу об'єкта, но объявлена как внешняя функция.
+ * Контекст методу об'єкта, але оголошено як зовнішню функцію.
  */
 
-function showTag() {
-  console.log('showTag -> this', this);
-  console.log('showTag -> this.tag', this.tag);
-}
-
-showTag();
-
-const mango = {
-  tag: 'Mango',
-};
-
-mango.showUserTag = showTag;
-console.log('mango', mango);
-
-mango.showUserTag();
-
 /**
- * Вызов без контекста, но объявлена как метод объекта.
+ * Виклик без контексту, але оголошено як метод об'єкта.
  */
 
-const poly = {
-  tag: 'Poly',
-  showTag() {
-    console.log('showTag -> this', this);
-    console.log('showTag -> this.tag', this.tag);
-  },
-};
-
-poly.showTag();
-
-const outerShowTag = poly.showTag;
-
-outerShowTag();
-
 /**
- * Контекст в callback-функциях
+ * Контекст у callback-функціях
  */
-
-const jacob = {
-  tag: 'Jacob',
-  showTag() {
-    console.log('showTag -> this', this);
-    console.log('showTag -> this.tag', this.tag);
-  },
-};
-
-function invokeAction(action) {
-  console.log(action);
-  action();
-}
-
-invokeAction(jacob.showTag);
--- src/js/02-tasks.js
+++ src/js/02-tasks.js
@@ -1,46 +1,46 @@
 function foo() {
-  console.log("foo -> this", this);
+  console.log('foo -> this', this);
 }
 
-foo(); // Какой this ???
+foo(); // Який this???
 
 /**
  * -------------------------
  */
 const book = {
-  title: "React for beginners",
+  title: 'React for beginners',
   showThis() {
-    console.log("showThis -> this", this);
+    console.log('showThis -> this', this);
   },
   showTitle() {
-    console.log("showTitle -> this.title", this.title);
+    console.log('showTitle -> this.title', this.title);
   },
 };
 
-book.showThis(); // Какой this ???
+book.showThis(); // Який this???
 
 const outerShowThis = book.showThis;
-outerShowThis(); // Какой this ???
+outerShowThis(); // Який this???
 
 const outerShowTitle = book.showTitle;
-outerShowTitle(); // Какой this ???
+outerShowTitle(); // Який this???
 
 /**
  * Напишіть метод calcTotalPrice(stoneName), який приймає назву каменю і
  * розраховує та повертає загальну вартість каменів з таким ім'ям, ціною та
- * кількістю з властивості stones.
+ * Кількість з властивості stones.
  */
 const chopShop = {
   stones: [
-    { name: "Emerald", price: 1300, quantity: 4 },
-    { name: "Diamond", price: 2700, quantity: 3 },
-    { name: "Sapphire", price: 1400, quantity: 7 },
-    { name: "Ruby", price: 800, quantity: 2 },
+    { name: 'Emerald', price: 1300, quantity: 4 },
+    { name: 'Diamond', price: 2700, quantity: 3 },
+    { name: 'Sapphire', price: 1400, quantity: 7 },
+    { name: 'Ruby', price: 800, quantity: 2 },
   ],
   calcTotalPrice(stoneName) {},
 };
 
-console.log(chopShop.calcTotalPrice("Emerald")); // 5200
-console.log(chopShop.calcTotalPrice("Diamond")); // 8100
-console.log(chopShop.calcTotalPrice("Sapphire")); // 9800
-console.log(chopShop.calcTotalPrice("Ruby")); // 1600
+console.log(chopShop.calcTotalPrice('Emerald')); // 5200
+console.log(chopShop.calcTotalPrice('Diamond')); // 8100
+console.log(chopShop.calcTotalPrice('Sapphire')); // 9800
+console.log(chopShop.calcTotalPrice('Ruby')); // 1600
