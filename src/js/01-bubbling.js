6f1606382f781c4eb768f75da75f6880d1313977
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-01-09 22:20:39 +0200

    update

--- src/js/01-bubbling.js
+++ src/js/01-bubbling.js
@@ -4,6 +4,29 @@
  * event.currentTarget - поточний елемент, на слухачі якого спіймали подію
  */
 
-const parent = document.querySelector("#parent");
-const child = document.querySelector("#child");
-const innerChild = document.querySelector("#inner-child");
+const parent = document.querySelector('#parent');
+const child = document.querySelector('#child');
+const innerChild = document.querySelector('#inner-child');
+
+parent.addEventListener('mouseEnter', e => {
+  console.log(e.currentTarget);
+  console.log(e.target);
+});
+
+child.addEventListener('click', e => {
+  console.log(e.currentTarget);
+  console.log(e.target);
+});
+
+innerChild.addEventListener('click', e => {
+  console.log('Гоп-стоп 1');
+});
+
+innerChild.addEventListener('click', e => {
+  console.log('Гоп-стоп 2');
+  e.stopImmediatePropagation();
+});
+
+innerChild.addEventListener('click', e => {
+  console.log('Гоп-стоп 3');
+});
---
+++ src/js/test.js
@@ -0,0 +1,7 @@
+function hello() {
+  console.log('HELLO WORLD');
+}
+
+class User {}
+
+const x = 123;
